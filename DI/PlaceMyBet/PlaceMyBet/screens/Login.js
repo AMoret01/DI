ed688a4346c6950c567eff197a6cff45c79174d2
ALEJANDRO MORET GOMEZ <almogo01@floridauniversitaria.es>
2020-11-26 16:45:15 +0100

    PlaceMyBet pantallas

---
+++ DI/PlaceMyBet/PlaceMyBet/screens/Login.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export class Login extends Component{
+
+    render(){
+        return(
+            <View>
+                <Text> Login</Text>
+            </View>
+        )
+    }
+}
